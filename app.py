0aeb8b29fdf08ee33da27d9bdad349af70b16ee6
boujellal4-boop <boujellal4@gmail.com>
2025-12-10 20:02:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ industries = [
 technologies = [
     'high-end addressable system',
     'mid-end addressable',
-    'conventional','wireless','aspirating smoke detection','linear heat detection','flame detection','evacuation' ,'general'
+    'conventional','wireless','aspirating smoke detection','linear heat detection','flame detection','evacuation','general'
 ]
 
 # ====== Estimation parameters ======
@@ -37,7 +37,7 @@ tech_base_values = {
     'conventional': 7000,
     'linear heat detection': 6000,
     'flame detection': 5000,
-    'general': 14,572
+    'general': 14572
 }
 
 lead_type_multiplier = {'end-user':1.2,'distributor':6,'installer':3.3,'consultant':1.95}
